fee0a9ea391e03b6ea6f42d92d5b03b433348215
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-19 00:25:36 -0700

    Added a try catch statement and error message if user enters a non allowed command

--- SlingshotTakeHomeChallengeCli/src/cli.js
+++ SlingshotTakeHomeChallengeCli/src/cli.js
@@ -2,38 +2,56 @@ const arg = require("arg");
 
 export function cli(originalArguments) {
   let options = convertArguements(originalArguments);
-  console.log(options);
+  if (options["error"]) {
+    error("Error : Please enter an allowed command", true);
+  } else {
+    console.log("Allowed Prompt");
+  }
 }
 
 function convertArguements(originalArguments) {
-  const args = arg(
-    {
-      "--add": Boolean,
-      "--delete": Boolean,
-      "--search": Boolean,
-      "--autocomplete": Boolean,
-      "--display": Boolean,
-    },
-    //the first 2 raw arguments are file paths we dont need so we take them off
-    { argv: originalArguments.slice(2) }
-  );
-  return {
-    add: args["--add"] || false,
-    delete: args["--delete"] || false,
-    search: args["search"] || false,
-    autocomplete: args["autocomplete"] || false,
-    display: args["display"] || false,
-    word: args._[0],
-  };
+  try {
+    const args = arg(
+      {
+        "--add": Boolean,
+        "--delete": Boolean,
+        "--search": Boolean,
+        "--autocomplete": Boolean,
+        "--display": Boolean,
+      },
+      //the first 2 raw arguments are file paths we dont need so we take them off
+      { argv: originalArguments.slice(2) }
+    );
+    return {
+      add: args["--add"] || false,
+      delete: args["--delete"] || false,
+      search: args["search"] || false,
+      autocomplete: args["autocomplete"] || false,
+      display: args["display"] || false,
+      word: args._[0],
+    };
+  } catch (err) {
+    return { error: true };
+  }
+}
+
+//error function in case of any bad user input
+function error(message, includeHelpCommand = false) {
+  console.error(message);
+  if (includeHelpCommand) {
+    console.log("");
+    helpCommands();
+  }
 }
 
 //help function to help users navigate the cli  (ignored by prettier formatter for formatting reasons)
 // prettier-ignore
 function helpCommands() {
+  console.log("All Possible Commands For The Trie: ")
   console.log("--add                    adds a word to the trie");
   console.log("--delete                 deletes a word from the trie");
   console.log("--search                 searches a trie for a word");
-  console.log("--autocomplete           gives a list of words in the trie given a prefix");
+  console.log("--autocomplete           gives a list of prefix included words in the trie given a prefix");
   console.log("--display                prints the trie out");
   console.log("");
 }
