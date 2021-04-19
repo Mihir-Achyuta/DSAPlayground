975a7ddec5226bfd43d433704ac75e9eeaea9f6a
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-18 23:06:00 -0700

    Added options for cli

--- SlingshotTakeHomeChallengeCli/src/cli.js
+++ SlingshotTakeHomeChallengeCli/src/cli.js
@@ -1,3 +1,26 @@
-export function cli(arguements) {
-  console.log(arguements);
+const arg = require("arg");
+
+export function cli(originalArguements) {
+  let options = convertArguements(originalArguements);
+  console.log(options);
+}
+
+function convertArguements(originalArguements) {
+  const args = arg(
+    {
+      "--add": Boolean,
+      "--delete": Boolean,
+      "--autocomplete": Boolean,
+      "--search": Boolean,
+      "--display": Boolean,
+    },
+    { argv: originalArguements.slice(2) }
+  );
+  return {
+    add: args["--add"] || false,
+    delete: args["--delete"] || false,
+    autocomplete: args["autocomplete"] || false,
+    search: args["search"] || false,
+    display: args["display"] || false,
+  };
 }
