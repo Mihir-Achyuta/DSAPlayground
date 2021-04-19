2ac0d5e316e7ee7dd3d9543284ed12cd76364d53
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-18 23:53:13 -0700

    Added cli help function

--- SlingshotTakeHomeChallengeCli/src/cli.js
+++ SlingshotTakeHomeChallengeCli/src/cli.js
@@ -1,26 +1,39 @@
 const arg = require("arg");
 
-export function cli(originalArguements) {
-  let options = convertArguements(originalArguements);
+export function cli(originalArguments) {
+  let options = convertArguements(originalArguments);
   console.log(options);
 }
 
-function convertArguements(originalArguements) {
+function convertArguements(originalArguments) {
   const args = arg(
     {
       "--add": Boolean,
       "--delete": Boolean,
-      "--autocomplete": Boolean,
       "--search": Boolean,
+      "--autocomplete": Boolean,
       "--display": Boolean,
     },
-    { argv: originalArguements.slice(2) }
+    //the first 2 raw arguments are file paths we dont need so we take them off
+    { argv: originalArguments.slice(2) }
   );
   return {
     add: args["--add"] || false,
     delete: args["--delete"] || false,
-    autocomplete: args["autocomplete"] || false,
     search: args["search"] || false,
+    autocomplete: args["autocomplete"] || false,
     display: args["display"] || false,
+    word: args._[0],
   };
+}
+
+//help function to help users navigate the cli  (ignored by prettier formatter for formatting reasons)
+// prettier-ignore
+function helpCommands() {
+  console.log("--add                    adds a word to the trie");
+  console.log("--delete                 deletes a word from the trie");
+  console.log("--search                 searches a trie for a word");
+  console.log("--autocomplete           gives a list of words in the trie given a prefix");
+  console.log("--display                prints the trie out");
+  console.log("");
 }
