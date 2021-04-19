2efe8f5a33925b1d4dc704cd6fe16915ee6ea538
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-18 21:56:10 -0700

    Made server listen on port 3001 and returned mock json responses

--- SlingshotTakeHomeChallengeServer/server.js
+++ SlingshotTakeHomeChallengeServer/server.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const app = express();
 
+app.listen(3001, function () {
+  console.log("Listening on Port 3001");
+});
+
 app.post("/add", function (req, res) {
   console.log("Added");
+  res.json({ added: true });
 });
 
 app.post("/delete", function (req, res) {
   console.log("Deleted");
+  res.json({ deleted: true });
 });
 
 app.get("/search", function (req, res) {
   console.log("Searched");
+  res.json({ searched: true });
 });
 
 app.get("/autocomplete", function (req, res) {
   console.log("Autocompleted words");
+  res.json({ autocompleted: true });
 });
 
 app.get("/display", function (req, res) {
   console.log("Displayed");
+  res.json({ displayed: true });
 });
