32bec5224051c62429ce331a5633e50241443cf2
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-19 21:55:09 -0700

    Added rough add method for trie

--- SlingshotTakeHomeChallengeServer/trie.js
+++ SlingshotTakeHomeChallengeServer/trie.js
@@ -5,15 +5,34 @@ class TrieNode {
   constructor(letter) {
     this.letter = letter;
     this.children = {};
-    this.isEnd = Object.keys(this.children).length > 0 ? false : true;
+    this.isEnd = true;
   }
 }
 
 class Trie {
-  constructor() {}
-  add(word) {}
+  constructor() {
+    this.rootNode = new TrieNode(null);
+  }
+  add(word) {
+    let currNode = this.rootNode;
+    for (let i of word) {
+      //if there is no children for the ith letter :
+      //1. set the current Node end to false(it will have a child now)
+      //2. the children[letter] of that node is a new Trie Node of the ith letter
+      if (!currNode["children"][i]) {
+        currNode["isEnd"] = false;
+        currNode["children"][i] = new TrieNode(i);
+      }
+      currNode = currNode["children"][i];
+    }
+  }
   delete(word) {}
   search(word) {}
   autocomplete(word) {}
   display() {}
 }
+
+let trie = new Trie();
+trie.add("cat");
+trie.add("car");
+trie.add("corn");
