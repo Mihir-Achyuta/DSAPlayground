d0513eff25dcca1a168c5ec59a90534dff569eb7
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-19 17:39:43 -0700

    Changed axios request from async to .then.catch

--- SlingshotTakeHomeChallengeCli/src/cli.js
+++ SlingshotTakeHomeChallengeCli/src/cli.js
@@ -47,29 +47,33 @@ export function cli(originalArguments) {
 }
 
 //executes a specific operation on the trie and gets back the result
-async function trieOperation(command, word) {
+function trieOperation(command, word) {
   if (command === "display") {
-    try {
-      let results = await axios.get(`http://localhost:3001/${command}`);
-      if (results["data"]["succeeded"]) {
-        console.log(`Trie Operation ${command} Succeeded`);
-        console.log(results["data"]["message"]);
-      }
-    } catch (err) {
-      error("Error : Was not able to connect to the server");
-    }
-  } else {
-    try {
-      let results = await axios.post(`http://localhost:3001/${command}`, {
-        specifiedWord: word,
+    axios
+      .get(`http://localhost:3001/${command}`)
+      .then(function (results) {
+        if (results["data"]["succeeded"]) {
+          console.log(`Trie Operation ${command} Succeeded`);
+          console.log(results["data"]["message"]);
+        }
+      })
+      .catch(function (err) {
+        error("Error : Was not able to connect to the server");
       });
-      if (results["data"]["succeeded"]) {
-        console.log(`Trie Operation ${command} Succeeded`);
-        console.log(results["data"]["message"]);
-      }
-    } catch (err) {
-      error("Error : Was not able to connect to the server");
-    }
+  } else {
+    axios
+      .post(`http://localhost:3001/${command}`, {
+        specifiedWord: word,
+      })
+      .then(function (results) {
+        if (results["data"]["succeeded"]) {
+          console.log(`Trie Operation ${command} Succeeded`);
+          console.log(results["data"]["message"]);
+        }
+      })
+      .catch(function (err) {
+        error("Error : Was not able to connect to the server");
+      });
   }
 }
 
