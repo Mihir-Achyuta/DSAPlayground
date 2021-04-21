5119f3e2e747b692a4809caf74044227f342d251
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-21 01:21:04 -0700

    Refactored search method to only allow words and not prefixes or words

--- SlingshotTakeHomeChallengeServer/trie.js
+++ SlingshotTakeHomeChallengeServer/trie.js
@@ -14,11 +14,12 @@ class Trie {
     this.rootNode = new TrieNode(null);
   }
 
-  //this add method allows for multiple words of the same prefix to be considered as words as needed
+  //this add method adds words to the trie and allows for multiple words of the same prefix to be considered as words as needed
   //for example if we inserted "cat" and then "cats" right after, "cat" would not be overridden by "cats" as a word (both are considered words)
   //the word end property set to true for "cat" would still maintain its relative role as a word
   add(word) {
     let currNode = this.rootNode;
+
     for (let i of word) {
       //if there is no children for the ith letter :
       //1. set the current Node end to false(it will have a child now)
@@ -34,9 +35,13 @@ class Trie {
 
   delete(word) {}
 
+  //the search method allows the user to see if a WORD is in the trie not a prefix
+  //if "pasta" is in the trie then the method returns true if "pasta" was searched but false if "pas" was searched
+  //"pas" is not a word in the trie, only a prefix in the word "pasta"
   search(word) {
     let currNode = this.rootNode;
     let found = true;
+
     for (let i of word) {
       //if there is no children for the ith letter :
       //the word does not exist in the trie so we exit out of the loop
@@ -46,6 +51,10 @@ class Trie {
       }
       currNode = currNode["children"][i];
     }
+    //if we do have a possible word, we need to check if the word is an actual defined word instead of a prefix (no false positives)
+    //if you search for "ca" in "cats" trie, it returns false as "ca" is not a word even though the letters are defined in "cats"
+    if (found) found = currNode["wordEnd"];
+
     console.log(
       `The word ${word} is ${found ? "found" : "not found"} in the trie`
     );
@@ -68,4 +77,5 @@ trie.add("car");
 trie.add("corn");
 trie.add("cob");
 trie.add("cats");
+trie.search("catss");
 console.log(JSON.stringify(trie.rootNode));
