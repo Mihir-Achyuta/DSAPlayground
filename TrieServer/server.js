6349187e54695ee78428a9e60be796cb3a477693
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-24 15:33:15 -0700

    Added clarifying comments for server

--- TrieServer/server.js
+++ TrieServer/server.js
@@ -1,21 +1,26 @@
+//imports express for routes, cors for sharing resources and being api pingable, and the trie data structure
 const express = require("express");
 const cors = require("cors");
 const { Trie } = require("./trie");
 const app = express();
 let trie = new Trie();
 
+//uses cors, and allows express to parse bodies and json in api requests
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//allows heroku to host server on its own port or 3001 on local
 app.listen(process.env.PORT || 3001, function () {
   console.log("Listening on Port 3001");
 });
 
+//confirmation that server is working
 app.get("/", function (req, res) {
   res.send("Trie Server is working");
 });
 
+//displays all the words in the trie and the trie structure in JSON
 app.get("/display", function (req, res) {
   let { words, structure } = trie.display();
 
@@ -25,6 +30,7 @@ app.get("/display", function (req, res) {
   });
 });
 
+//resets the trie to have no word along with the json database file
 app.get("/reset", function (req, res) {
   trie.reset();
 
@@ -34,6 +40,7 @@ app.get("/reset", function (req, res) {
   });
 });
 
+//adds a word to the trie
 app.post("/add", function (req, res) {
   let wordToAdd = req.body.specifiedWord;
 
@@ -41,6 +48,7 @@ app.post("/add", function (req, res) {
   res.json({ succeeded: true, message: `Word ${wordToAdd} Added to trie` });
 });
 
+//deletes a word from the trie if it is present
 app.post("/delete", function (req, res) {
   let wordToDelete = req.body.specifiedWord;
   let isDeleted = trie.delete(wordToDelete);
@@ -52,6 +60,7 @@ app.post("/delete", function (req, res) {
   });
 });
 
+//checks if a word is in the trie and lets user know result
 app.post("/search", function (req, res) {
   let wordToFind = req.body.specifiedWord;
   let wordFound = trie.search(wordToFind);
@@ -64,6 +73,7 @@ app.post("/search", function (req, res) {
   });
 });
 
+//gets all the words in the trie starting with a prefix
 app.post("/autocomplete", function (req, res) {
   let wordToAutocomplete = req.body.specifiedWord;
   let { found, words } = trie.autocomplete(wordToAutocomplete);
