8d1b865beafe952cd205b555fb34f64472ec17a2
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-04-24 21:02:01 -0700

    Added names for all 5 tests and added first one

--- TrieServer/trieTests.js
+++ TrieServer/trieTests.js
@@ -1,11 +1,60 @@
-//these tests will incorporate all 5 methods but gradually incorporate each one per test
+//these tests will incorporate all 5 methods(add, delete, search, autocomplete, display) but gradually incorporate each one per test
+//these tests will be global and test the global state as preferred in the requirements
+const axios = require("axios");
 
-function test1() {}
+//should expect 5 words to be added in the trie
+//if a request succeeds then the word will be added
+async function addFiveAndGetFive() {
+  let wordArray = ["This", "is", "one", "nice", "test"];
+  let requestsSuceeded = true;
 
-function test2() {}
+  await resetTrie();
+  console.log("Test 1 Started");
+  for (let i of wordArray) {
+    try {
+      let results = await axios.post(
+        `https://triechallenge.herokuapp.com/add`,
+        {
+          specifiedWord: i,
+        }
+      );
+      console.log(results["data"]["message"]);
+    } catch (error) {
+      requestsSuceeded = false;
+    }
+  }
 
-function test3() {}
+  console.log(`All 5 Requests Succeeded? ${requestsSuceeded}`);
+  console.log("Test 1 Ended");
+  console.log("");
+}
 
-function test4() {}
+//should expect 4 words to be added in the trie
+function addFiveAndDeleteOne() {}
 
-function test5() {}
+//should expect 3 words to be added in the trie and the last added word to be found
+function addFiveAndDeleteTwoAndSearchTrue() {}
+
+function addFiveAndDeleteTwoAndSearchFalse() {}
+
+function addFiveAndDeleteTwoAndAuto() {}
+
+function addFiveAndDeleteTwoAndDisplay() {}
+
+//resets trie as needed
+async function resetTrie() {
+  try {
+    let results = await axios.get(`https://triechallenge.herokuapp.com/reset`);
+    console.log(results["data"]["message"]);
+  } catch (error) {
+    console.log("Trie not reset");
+  }
+}
+
+//uncomment each test as needed
+// addFiveAndGetFive();
+// addFiveAndDeleteOne();
+// addFiveAndDeleteTwoAndSearchTrue();
+// addFiveAndDeleteTwoAndSearchFalse();
+// addFiveAndDeleteTwoAndAuto();
+// addFiveAndDeleteTwoAndDisplay();
