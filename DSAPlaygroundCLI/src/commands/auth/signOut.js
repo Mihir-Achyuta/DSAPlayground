33c0e27cd08d7597f6909d2e9adea7bead686e2c
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-08-06 20:08:23 -0700

    Added sign out command for cli

--- DSAPlaygroundCLI/src/commands/auth/signOut.js
+++ DSAPlaygroundCLI/src/commands/auth/signOut.js
@@ -3,10 +3,16 @@ const { errorHandler } = require("../../handlers/error/errorHandler");
 
 async function signOut() {
   try {
-    const { data } = await axios.get("http://localhost:3001/currentuser");
+    const { data } = await axios.default.get(
+      "http://localhost:3001/currentuser"
+    );
 
     if (data["results"] !== null) {
-      //make user sign up
+      const userData = await axios.default.post(
+        "http://localhost:3001/signout"
+      );
+
+      console.log(userData.data.message);
     } else {
       errorHandler(data["message"]);
     }
