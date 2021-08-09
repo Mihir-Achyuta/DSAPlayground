781241e4d0b35169973c7c680c9909e8639ae2a7
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-08-09 14:31:04 -0700

    Added boilerplate stack handlers

---
+++ DSAPlaygroundServer/handlers/stackHandlers.js
@@ -0,0 +1,12 @@
+const { Stack } = require("../models/stack");
+const { getUserData } = require("./userDataHandlers");
+
+function displayStack(req, res) {}
+
+function push(req, res) {}
+
+function pop(req, res) {}
+
+function deleteStack(req, res) {}
+
+module.exports = { displayStack, push, pop, deleteStack };
