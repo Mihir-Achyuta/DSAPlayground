674d0b34acaae3d251771c8e59a9e0ab8068ef0c
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-08-10 00:33:32 -0700

    Added stack delete handler for server

--- DSAPlaygroundServer/handlers/stackHandlers.js
+++ DSAPlaygroundServer/handlers/stackHandlers.js
@@ -68,7 +68,38 @@ function pushStack(req, res) {}
 
 function popStack(req, res) {}
 
-function deleteStack(req, res) {}
+function deleteStack(req, res) {
+  getUserData()
+    .get()
+    .then((doc) => {
+      const { currentData } = doc.data();
+      const stackFound = currentData["stack"].find(
+        (stack) => stack["name"] === req.params.name
+      );
+
+      if (stackFound) {
+        currentData["stack"] = currentData["stack"].filter(
+          (stack) => queue["stack"] !== req.params.name
+        );
+        getUserData().set({ currentData });
+
+        res.json({
+          message: `Deleted stack with name ${req.params.name}`,
+          error: false,
+          code: 200,
+          results: stackFound,
+        });
+      } else {
+        res.json({
+          message: `Stack with name ${req.params.name} doesn't exist`,
+          error: false,
+          code: 200,
+          results: stackFound,
+        });
+      }
+    })
+    .catch((error) => console.log(error));
+}
 
 module.exports = {
   displayStack,
