d0ac91731621612febac58ef270949ea773b239c
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-08-10 00:27:57 -0700

    Added stack creation handler

--- DSAPlaygroundServer/handlers/stackHandlers.js
+++ DSAPlaygroundServer/handlers/stackHandlers.js
@@ -28,7 +28,41 @@ function displayStack(req, res) {
     });
 }
 
-function createStack(req, res) {}
+function createStack(req, res) {
+  getUserData()
+    .get()
+    .then((doc) => {
+      const { currentData } = doc.data();
+      const isDuplicate = currentData["stack"].find(
+        (stack) => stack["name"] === req.params.name
+      );
+
+      if (isDuplicate) {
+        res.json({
+          message: `Stack with name ${req.params.name} already exists`,
+          error: false,
+          code: 200,
+          results: isDuplicate,
+        });
+      } else {
+        currentData["stack"].push({
+          name: req.params.name,
+          data: [],
+        });
+        getUserData().set({ currentData });
+        res.json({
+          message: `Stack with name ${req.params.name} created`,
+          error: false,
+          code: 200,
+          results: {
+            name: req.params.name,
+            data: [],
+          },
+        });
+      }
+    })
+    .catch((error) => console.log(error));
+}
 
 function pushStack(req, res) {}
 
