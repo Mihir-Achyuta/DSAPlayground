da48a6700f6f3a9b7094cafb225c0e9fa8a3d36d
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-08-10 23:48:12 -0700

    Added binary heap help command

---
+++ DSAPlaygroundCLI/src/commands/help/dataStructureHelp/binaryHeapHelp.js
@@ -0,0 +1,16 @@
+// prettier-ignore
+function binaryHeapHelp() {
+    console.log("");
+    console.log("Commands are done by data_structure_name data_structure_command (and data_structure_value if needed) for all commands");
+    console.log("All Possible Binary Heap Commands: ");
+    console.log("exit                                    exits the DSACli");
+    console.log("back_playground                         navigates back to the playground cli");
+    console.log("display                                 displays the binary heap");
+    console.log("create                                  creates a new binary heap");
+    console.log("insert                                  inserts a value into the binary heap");
+    console.log("extract                                 extracts the maximum value from the binary heap");
+    console.log("delete                                  deletes the binary heap");
+    console.log("");
+}
+
+module.exports = { binaryHeapHelp };
--- DSAPlaygroundCLI/src/commands/help/playgroundHelp.js
+++ DSAPlaygroundCLI/src/commands/help/playgroundHelp.js
@@ -3,12 +3,13 @@ function playgroundHelp() {
     console.log("");
     console.log("Commands are done by command_name for all commands");
     console.log("All Possible Playground Commands: ");
+    console.log("exit                                 exits the DSACli");
+    console.log("back_home                            navigates back to the home cli")
     console.log("stack                                navigates to the Stack Playground");
     console.log("queue                                navigates to the Queue Playground");
     console.log("binary_search_tree                   navigates to the Binary Search Tree Playground");
     console.log("binary_heap                          navigates to the Binary Heap Playground");
     console.log("trie                                 navigates to the Trie Playground");
-    console.log("exit                                 exits the DSACli");
     console.log("");
 }
 
