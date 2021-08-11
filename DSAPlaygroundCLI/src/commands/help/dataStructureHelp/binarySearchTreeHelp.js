f9135054d26d2cdfd033fe39159a52ebf5077125
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-08-11 01:05:36 -0700

    Added bianry search tree help command for cli

---
+++ DSAPlaygroundCLI/src/commands/help/dataStructureHelp/binarySearchTreeHelp.js
@@ -0,0 +1,16 @@
+// prettier-ignore
+function binarySearchTreeHelp() {
+    console.log("");
+    console.log("Commands are done by data_structure_name data_structure_command (and data_structure_value if needed) for all commands");
+    console.log("All Possible Binary Search Tree Commands: ");
+    console.log("exit                                    exits the DSACli");
+    console.log("back_playground                         navigates back to the playground cli");
+    console.log("display                                 displays the binary search tree");
+    console.log("search                                  searches for a value for the binary search tree")
+    console.log("create                                  creates a new binary search tree");
+    console.log("insert                                  inserts a value into the binary search tree");
+    console.log("delete                                  deletes the binary heap");
+    console.log("");
+}
+
+module.exports = { binarySearchTreeHelp };
--- DSAPlaygroundCLI/src/commands/help/helpCommands.js
+++ DSAPlaygroundCLI/src/commands/help/helpCommands.js
@@ -2,5 +2,13 @@ const { welcomeHelp } = require("./interfaceHelp/welcomeHelp");
 const { playgroundHelp } = require("./interfaceHelp/playgroundHelp");
 
 const { binaryHeapHelp } = require("./dataStructureHelp/binaryHeapHelp");
+const {
+  binarySearchTreeHelp,
+} = require("./dataStructureHelp/binarySearchTreeHelp");
 
-module.exports = { welcomeHelp, playgroundHelp, binaryHeapHelp };
+module.exports = {
+  welcomeHelp,
+  playgroundHelp,
+  binaryHeapHelp,
+  binarySearchTreeHelp,
+};
